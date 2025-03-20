c2bd305b3996c99c50251eb010bf75f6897509d0
loading-content <56720934+loading-content@users.noreply.github.com>
2025-03-20 11:49:20 +0100

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const navbar = document.getElementById("navbar");
     if (navbar) {
       navbar.innerHTML = `
-        <link href="navbar.css" rel="stylesheet" type="text/css"/>
+        <link href="css/navbar.css" rel="stylesheet" type="text/css"/>
         <ul>
             <li> <a href="index.html">home</a> </li>
             <li> <a href="projects.html">projects</a> </li>
